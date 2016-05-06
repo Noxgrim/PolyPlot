f988e361ca9a27c408eaccde748d8c0d3246e437
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2016-05-06 02:51:34 +0200

    Add Help window
    CheatSheet
    + Add help window and help texts
    
    DrawableComponent
    * Change typo of DrawableComponent to an abstract class and
      centralize colors
    + Add 'Hideable' interface to indicate that a component can be
      hidden.
    * Change InfoBox and DebugGui to be Hideable
    
    FuctionPlotter
    * Made InfoBox, DebugGui and CheatSheet to members of the FuctionPlotter
    + Add 'getBounds()' method
    + Add key binding for CheatSheet and InfoBox
    * Change key binding of debug GUI to not require 'Ctrl' anymore
    
    InfoBox
    * Change InfoBox to work with the provided bounds of the FunctionPlotter
      instead of the absolute ones to make components that use a part of
      the screen possible

---
+++ src/polyplot/graphics/Hideable.java
@@ -0,0 +1,13 @@
+package polyplot.graphics;
+
+/**
+ * @author Noxgrim
+ */
+public interface Hideable {
+
+    void setHidden(boolean hidden);
+
+    boolean isHidden();
+
+    void toggleHidden();
+}
